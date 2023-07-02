340976c08ce8b180baec990770f6bd5ddd8a7976
mahirnpatel <mahirpatel3107@gmail.com>
2023-07-02 19:59:08 +0530

    Solution of problem number 2057

---
+++ lib/List_solutions/smallest_index_with_equal_value_2057.ex
@@ -0,0 +1,13 @@
+defmodule SmallestIndex do
+  @spec smallest_equal(nums :: [integer]) :: integer
+  def smallest_equal(nums) do
+    r =
+      nums
+      |> Enum.with_index()
+      |> Enum.find(fn {n, i} ->
+        rem(i, 10) == n
+      end)
+
+    if r == nil, do: -1, else: elem(r, 0)
+  end
+end
