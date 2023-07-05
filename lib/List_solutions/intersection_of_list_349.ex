e29c8c17eff674f3bd92970b2c7c17545a50d586
mahirnpatel <mahirpatel3107@gmail.com>
2023-07-05 23:20:32 +0530

    Solution of problem number 349

---
+++ lib/List_solutions/intersection_of_list_349.ex
@@ -0,0 +1,7 @@
+defmodule IntersectionOfList do
+  @spec intersection(nums1 :: [integer], nums2 :: [integer]) :: [integer]
+  def intersection(nums1, nums2) do
+    MapSet.intersection(MapSet.new(nums1), MapSet.new(nums2))
+    |> MapSet.to_list()
+  end
+end
